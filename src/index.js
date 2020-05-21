363d8983188d2c234f245860d02412b11401a670
Carlos Raiol <caremanuel@gmail.com>
2020-05-21 11:59:22 -0300

    Introdução ao Redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './bootstrap.min.css';
+//import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { reducer } from './redux/reducers';
 
-const rootElement =  document.getElementById('app')
-ReactDOM.render(<App />, rootElement);
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(<Provider store={store}>
+    <App />
+</Provider>, document.getElementById('app'));
+
+//serviceWorker.unregister();
+
+/*const rootElement =  document.getElementById('app')
+ReactDOM.render(<App />, rootElement);*/
